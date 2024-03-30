4fb1d2e65f8463d46ff1eb1d3dd39c213265d8ed
LisSharik <112002658+LisSharik@users.noreply.github.com>
2024-03-29 19:44:04 -0500

    Delete src directory

--- src/data/httpClient.js
+++
@@ -1,9 +0,0 @@
-const apiUrl = "https://api.themoviedb.org/3";
-
-
-
-export function get(path) {
-    const apiKey = '188bd6678a62441e7ccc497076ec844e'; // Reemplaza 'tu_api_key' con tu API Key de TMDb
-    return fetch(apiUrl + path + `?api_key=${apiKey}`)
-        .then((result) => result.json());
-}
--- src/utils/getMovieImg.jsx
+++
@@ -1,6 +0,0 @@
-import placeholder from "../img/placeholder.png"
-
-export function getMovieImg(path, width){
-    return path ? `https://image.tmdb.org/t/p/w${width + path}`: placeholder;
-}
-
